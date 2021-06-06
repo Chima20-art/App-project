70ad657cef769985fc9e0455dc0a7b1bfe87e02f
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2021-06-06 15:22:12 +0300

    added index

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const App = () => {
-  const onConectDevice = () => {};
+  const [showBluetoothDevices, setShowBluetoothDevices] = useState(false);
+  const onConnectDevice = () => {
+    setShowBluetoothDevices(!showBluetoothDevices);
+  };
 
   return (
-    <View style={styles.b}>
-      <TouchableOpacity style={styles.button} onPress={onConectDevice}>
+    <View
+      style={
+        showBluetoothDevices == false ? styles.centerButton : styles.buttonUp
+      }>
+      <TouchableOpacity style={styles.button} onPress={onConnectDevice}>
         <Text style={styles.gaz}>Connect device with bluetooth </Text>
       </TouchableOpacity>
+      <View style={showBluetoothDevices == false ? styles.hide : styles.show}>
+        <Text style={styles.device}>Device1</Text>
+        <Text style={styles.device}>Device2</Text>
+        <Text style={styles.device}>Device3</Text>
+        <Text style={styles.device}>Device4</Text>
+        <Text style={styles.device}>Device5</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  b: {
+  centerButton: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
   },
   button: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  hide: {
+    display: 'none',
+  },
+  show: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
     flexDirection: 'column',
     alignItems: 'center',
     margin: 'auto',
+  },
+  device: {
+    padding: 20,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 2,
   },
   gaz: {
     display: 'flex',
